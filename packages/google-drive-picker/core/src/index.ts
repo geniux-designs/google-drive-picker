dd6842cf1ee57137c0fd65a60fb0fc64f7436c25
Haukur Mar Bodvarsson <haukurmar@gmail.com>
2024-11-11 21:22:23 +0100

    feat(google-drive-picker-core): added createPicker for GoogleDrivePicker
    - Added `createPicker` function to initialize and display the Google Drive Picker UI.
    - Supports configuration options like multi-select and customizable view types.
    - Utilizes OAuth token for secure file access.
    - Returns selected file details, providing a structured output for downstream use.

--- packages/google-drive-picker/core/src/index.ts
+++ packages/google-drive-picker/core/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./identity/initializeGoogleIdentityService";
+export * from "./picker/createPicker";
 export * from "./types";
---
+++ packages/google-drive-picker/core/src/types/picker.ts
@@ -0,0 +1,54 @@
+/**
+ * Type representing a selected Google Drive file.
+ */
+export type GoogleDriveFile = {
+	id: string;
+	serviceId: string;
+	mimeType: string;
+	name: string;
+	description?: string;
+	type: string;
+	lastEditedUtc: number;
+	iconUrl: string;
+	url: string;
+	embedUrl: string;
+	sizeBytes: number;
+	isShared: boolean;
+};
+
+/**
+ * Configuration for initializing the Google Drive Picker, with options for view type and multi-select.
+ */
+export type GoogleDrivePickerConfig = {
+	allowMultiSelect?: boolean;
+	viewId?: GoogleDrivePickerViewId;
+	scopes?: GoogleDriveScope[];
+};
+
+/**
+ * Available view types for the Google Drive Picker.
+ */
+export type GoogleDrivePickerViewId =
+	| "DOCS"
+	| "DOCS_IMAGES"
+	| "DOCS_VIDEOS"
+	| "SPREADSHEETS"
+	| "PRESENTATIONS"
+	| "FORMS"
+	| "FOLDERS"
+	| "PDFS";
+
+/**
+ * Enum representing the available scopes for the Google Drive API. (v3)
+ */
+export type GoogleDriveScope =
+	| "https://www.googleapis.com/auth/drive" // Full access to all Google Drive files
+	| "https://www.googleapis.com/auth/drive.appdata" // Access to app-specific configuration data in Google Drive
+	| "https://www.googleapis.com/auth/drive.apps.readonly" // View Google Drive apps
+	| "https://www.googleapis.com/auth/drive.file" // Limited access to the files the app uses or creates
+	| "https://www.googleapis.com/auth/drive.meet.readonly" // Access to files created or edited by Google Meet
+	| "https://www.googleapis.com/auth/drive.metadata" // Manage metadata for files in Google Drive
+	| "https://www.googleapis.com/auth/drive.metadata.readonly" // Read-only access to file metadata in Google Drive
+	| "https://www.googleapis.com/auth/drive.photos.readonly" // Read-only access to photos, videos, and albums in Google Photos
+	| "https://www.googleapis.com/auth/drive.readonly" // Read-only access to all Google Drive files
+	| "https://www.googleapis.com/auth/drive.scripts"; // Manage Google Apps Script
